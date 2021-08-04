c5e516ef2be181b6647538f30e60f0bc72721f36
Takahashi345345 <ty.private.add@gmail.com>
2021-08-04 17:44:09 +0900

    Toastを追加 【REPLAYボタンを押下するように促す(お知らせ)】

--- app/src/main/java/jp/wings/nikkeibp/a30countgame/GameOver.kt
+++ app/src/main/java/jp/wings/nikkeibp/a30countgame/GameOver.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class GameOver : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,9 @@ class GameOver : AppCompatActivity() {
 
         //変数宣言【idの取得(戻るボタン)】
         val btnReplay = findViewById<Button>(R.id.btnReplay)
+
+        //REPLAYボタンを押下するように促す【お知らせ】
+        Toast.makeText(this,"REPLAYボタンを押すと再戦！！",Toast.LENGTH_LONG).show()
 
         //ボタン押下時の処理【戻るボタン】
         btnReplay.setOnClickListener {
