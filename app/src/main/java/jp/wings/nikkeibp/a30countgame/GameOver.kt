829e0488774f3dc079a9ef5d01d250c7cc666139
Takahashi345345 <ty.private.add@gmail.com>
2021-08-04 16:21:53 +0900

    Initial commit

---
+++ app/src/main/java/jp/wings/nikkeibp/a30countgame/GameOver.kt
@@ -0,0 +1,23 @@
+package jp.wings.nikkeibp.a30countgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class GameOver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_over)
+
+        //変数宣言【idの取得(戻るボタン)】
+        val btnReplay = findViewById<Button>(R.id.btnReplay)
+
+        //ボタン押下時の処理【戻るボタン】
+        btnReplay.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/jp/wings/nikkeibp/a30countgame/MainActivity.kt
@@ -0,0 +1,90 @@
+package jp.wings.nikkeibp.a30countgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    //変数宣言【カウントの初期値】
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //変数宣言【idの取得(1～3の加算ボタン)】
+        val btnPlus1 = findViewById<Button>(R.id.btnPlus1)
+        val btnPlus2 = findViewById<Button>(R.id.btnPlus2)
+        val btnPlus3 = findViewById<Button>(R.id.btnPlus3)
+
+        //ボタン押下時の処理【1ボタン】
+        btnPlus1.setOnClickListener {
+
+            //カウントに1加算
+            count++
+
+            //加算後の条件処理
+            if (count >= 30){
+
+                //画面遷移
+                screenTransition()
+            }else{
+
+                //カウント変更
+                tvChange()
+            }
+        }
+
+        //ボタン押下時の処理【2ボタン】
+        btnPlus2.setOnClickListener {
+
+            //カウントに2加算
+            count += 2
+
+            //加算後の条件処理
+            if (count >= 30){
+
+                //画面遷移
+                screenTransition()
+            }else{
+
+                //カウント変更
+                tvChange()
+            }
+        }
+
+        //ボタン押下時の処理【3ボタン】
+        btnPlus3.setOnClickListener {
+
+            //カウントに3加算
+            count += 3
+
+            //加算後の条件処理
+            if (count >= 30){
+
+                //画面遷移
+                screenTransition()
+            }else{
+
+                //カウント変更
+                tvChange()
+            }
+        }
+    }
+
+    //画面遷移メソッド【MainActivity.ktを終了すると同時にGameOver.ktに画面遷移】
+    private fun screenTransition(){
+        val intent = Intent(this,GameOver::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    //カウント変更メソッド【押下した加算ボタンに応じてテキストに反映】
+    private fun tvChange(){
+        val tvCount = findViewById<TextView>(R.id.tvCount)
+        tvCount.text = count.toString()
+    }
+}
